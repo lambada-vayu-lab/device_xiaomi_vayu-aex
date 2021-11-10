9bf8945686674fd66fe9e01197f7537963306b20
Nauval Rizky <enuma.alrizky@gmail.com>
2021-11-10 22:58:47 +0700

    vayu: parts: Unify the app labeling
    XiaomiParts on your left, devicesettings on your right, DeviceParts on
    the other side. This is not good, rename them all back to DeviceParts
    just like the old day.
    
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -33,7 +33,7 @@ import org.lineageos.settings.thermal.ThermalUtils;
 public class BootCompletedReceiver extends BroadcastReceiver implements Controller {
 
     private static final boolean DEBUG = false;
-    private static final String TAG = "XiaomiParts";
+    private static final String TAG = "DeviceParts";
 
     @Override
     public void onReceive(final Context context, Intent intent) {
