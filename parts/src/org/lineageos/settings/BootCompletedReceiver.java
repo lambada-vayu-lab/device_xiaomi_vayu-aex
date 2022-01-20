347bb39a905b2f15e7017075ddabdbf86fa9064d
Lostark13 <tushar9915@gmail.com>
2022-01-20 14:54:47 +0530

    vayu: parts: Add per app maximum refresh rate
    -based on commits 9cd2ebf[1] , 87a43c0[2] , d795e4f[3]
    
    ref:
    [1] https://github.com/RavenOS-Devices/device_xiaomi_surya/commit/9cd2ebf76d86a64eccbc934ca72751398977e8bc
    [2] https://github.com/RavenOS-Devices/device_xiaomi_surya/commit/87a43c0f4170d23db01cd3f5cdb657130edd136b
    [3] https://github.com/RavenOS-Devices/device_xiaomi_surya/commit/d795e4f05cb31712e6c4b15760473316b1b4bd80
    
    Original commit:
    https://github.com/RavenOS-Devices/device_xiaomi_surya/commit/c8c200e44a0dae702c459597b48c5b1c5a3f5972
    
    [someone5678]
    * move the location from display settings to devicesettings
      now it's located under Refresh Rate category
    * add main icon
    * change theme to SubSettingsBase
    
    Signed-off-by: Lostark13 <tushar9915@gmail.com>
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: someone5678 <nemui3353@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -29,6 +29,7 @@ import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.haptic.HapticUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.refreshrate.RefreshUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver implements Controller {
 
@@ -71,5 +72,6 @@ public class BootCompletedReceiver extends BroadcastReceiver implements Controll
         DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
         HapticUtils.restoreLevel(context);
+        RefreshUtils.startService(context);
     }
 }
